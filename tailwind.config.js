d35412d6770bc2564f519f227502c604a625a5d0
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2025-03-09 22:51:40 +0300

    feat(website): all sections: header, price, services, expertise, team and foter

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#023e8a",
+        secondary: "#caf0f8",
+      },
+    },
+  },
+  plugins: [],
+};
